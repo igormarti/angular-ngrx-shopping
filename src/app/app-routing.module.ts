61477c85cc78130e2a4e317868fbd4c1498ec5d3
Igor Martins <igorm573@gmail.com>
2024-04-26 08:25:43 -0300

    commit changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'', redirectTo: 'products', pathMatch: 'full' 
+  },
+  {
+    path:'products',
+    loadChildren: () => import('./modules/product/product.module').then((m) => m.ProductModule)
+  },
+  {
+    path:'favorites',
+    loadChildren: () => import('./modules/favorite-product/favorite-product.module').then((m) => m.FavoriteProductModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
